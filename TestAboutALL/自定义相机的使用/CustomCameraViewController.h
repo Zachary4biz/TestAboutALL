ed954c20ecc953632c221800a4c9dffb8d5fc3c5
Zachary4biz <zachary4biz@yeah.net>
2017-06-01 23:03:45 +0800

    添加了自定义相机，未完成

---
+++ TestAboutALL/自定义相机的使用/CustomCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomCameraViewController.h
+//  TestAboutALL
+//
+//  Created by Zac on 2017/6/1.
+//  Copyright © 2017年 周桐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCameraViewController : UIViewController
+
+@end
---
+++ TestAboutALL/自定义相机的使用/CustomCameraViewController.m
@@ -0,0 +1,29 @@
+//
+//  CustomCameraViewController.m
+//  TestAboutALL
+//
+//  Created by Zac on 2017/6/1.
+//  Copyright © 2017年 周桐. All rights reserved.
+//
+
+#import "CustomCameraViewController.h"
+
+@interface CustomCameraViewController ()
+
+@end
+
+@implementation CustomCameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
